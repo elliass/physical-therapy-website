19242c766503d427120ade0420f6e48817c26207
elliass <rachidi.elliass@gmail.com>
2021-07-13 21:07:24 +0200

    Added Hero section

---
+++ src/components/Hero.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { makeStyles } from "@material-ui/core"
+import '@fontsource/roboto';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
+import StyledButton from "./StyledButton" 
+
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    backgroundColor: theme.palette.primary.main,
+    height: '100vh',
+  },
+  grid: {
+    maxWidth: '1160px',
+    margin: '0 auto',
+    // paddingTop: '13em',
+    height: '100%',
+    maxHeight: '90vh',
+  },
+  gridLeft: {
+    margin: 'auto 0',
+  },
+  gridRight: {
+    margin: 'auto 0',
+    textAlign: 'right',
+  },
+  title: {
+    paddingBottom: '50px'
+  },
+  paragraph: {
+    padding: '0 10em 4em 0',
+  },
+  btn: {
+    padding: '.8em 2em', 
+  },
+  image: {
+    maxWidth: '100%',
+  }
+}))
+
+export default function Hero() {
+    const classes = useStyles()
+
+    return (
+        <div className={classes.container}>
+            <Grid className={classes.grid} container> 
+                <Grid className={classes.gridLeft} item md={6}>
+                  <Typography className={classes.title} variant="h2" component="h1">Kinésithérapie à <br/> votre domicile</Typography>
+                  <Container className={classes.paragraph}>
+                    <Typography variant="body1">
+                    Vous avez besoin de séances de kinésithérapie à domicile ?
+                    Notre spécialiste se déplace jusque chez vous pour vous soigner.
+                    </Typography>
+                  </Container>
+                  {/* <StyledButton>Prendre Rendez-Vous</StyledButton> */}
+                  <Button className={classes.btn} variant="contained" color="secondary" endIcon={<ArrowForwardIosIcon />}>Prendre Rendez-Vous</Button>
+                </Grid>
+                <Grid className={classes.gridRight} item md={6}>
+                  <img  className={classes.image} src="/injured-banner.svg" alt="site banner" />
+                </Grid>
+            </Grid>
+        </div>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import { createTheme, ThemeProvider } from "@material-ui/core/styles"
 // import { deepPurple } from "@material-ui/core/colors"
 import Navbar from "../components/Navbar"
+import Hero from "../components/Hero"
 
 const theme = createTheme({
   palette: {
@@ -11,6 +12,16 @@ const theme = createTheme({
     secondary: {
       main: '#820FF0'
     },
+  },
+  typography: {
+    h2: {
+      fontWeight: 700,
+      fontSize: "4rem",
+      letterSpacing: '0.05em'
+    },
+    body1: {
+      letterSpacing: '0.09em'
+    }
   }
 })
 
@@ -18,6 +29,7 @@ export default function Home() {
   return <div>
     <ThemeProvider theme={theme}>
       <Navbar />
+      <Hero />
     </ThemeProvider>
     </div>
 }
