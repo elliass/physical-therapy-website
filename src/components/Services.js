935e71986badc8109bb08fa006e12475f4f31443
elliass <rachidi.elliass@gmail.com>
2021-07-17 01:24:33 +0200

    Added images for each service

--- src/components/Services.js
+++ src/components/Services.js
@@ -7,7 +7,7 @@ import Service from './Service'
 const useStyles = makeStyles((theme) => ({
     container: {
         backgroundColor: 'white',
-        height: '100vh',
+        height: '300vh',
       },
     subContainer: {
         // border: 'solid 1px black',
@@ -25,29 +25,32 @@ export default function Services() {
             id: 1,
             title: 'Pathologies traitées',
             description: 'Une réeducation préventive et/ou curative vous sera proposée. Nous prenons en charge les problèmes orthopédiques (articulaire, musculaire osseux). Ainsi que les pathologies neurologiques et cardio-pulmonaires.',
+            image: '/body-icons.svg',
             last: false,
         },
         {
             id: 2,  
             title: 'Prise en charge',
             description: 'Le thérapeute utilise un pannel de techniques. Les traitements seront réalisés sur base de méthodes acquises au cours de différentes spécialisations. Le principe étant de s’adapter aux différents types de patients rencontrés.',
+            image: '/physical-therapy.svg',
             last: false,
         },
         {
             id: 3,  
             title: 'En restant chez vous',
             description: 'Notre spécialiste se déplace chez vous pour vous aider à vous remettre de vos douleureuses blessures ou à récupérer après une intervention chirurgicale.',
+            image: '/home.svg',
             last: false,
         },
      ])
 
     return (
         <div className={classes.container}>
-            {services.map((service) => (
             <div className={classes.subContainer}>
+                {services.map((service) => (
                 <Service key={service.id} service={service}/>
+                ))}
             </div>
-            ))}
         </div>
     )
 }
