63abdd3c1194ed02ce5d5d1ba10865b1a0b06ba6
elliass <rachidi.elliass@gmail.com>
2021-07-17 15:11:53 +0200

    Completed Treatments component first version

--- src/components/Treatments.js
+++ src/components/Treatments.js
@@ -3,11 +3,12 @@ import { makeStyles } from "@material-ui/core"
 import '@fontsource/roboto'
 import Typography from "@material-ui/core/Typography"
 import Grid from "@material-ui/core/Grid"
+import { BorderColor } from '@material-ui/icons'
 
 const useStyles = makeStyles((theme) => ({
     container: {
         backgroundColor: '#191919',
-        height: '150vh',
+        height: '180vh',
         position: 'relative',
     },
     whiteContainer: {
@@ -39,6 +40,17 @@ const useStyles = makeStyles((theme) => ({
         right: '0',
         // display: 'flex'
     },
+    bottomContainer: {
+        backgroundColor: 'white',
+        opacity: '.01',
+        position: 'absolute',
+        // content: '',
+        width: '100%',
+        height: '150px',
+        bottom: '0px',
+        right: '0',
+        // display: 'flex'
+    },
 
     titleContainer: {
         display: 'flex',
@@ -56,29 +68,36 @@ const useStyles = makeStyles((theme) => ({
         right: '8em',
         maxWidth: '1160px',
         width: '100%',
-        height: '100%',
+        // height: '100%',
         // margin: '0 auto',
     },
     grayContainer:{
         // backgroundColor: 'gray',
+        position: 'absolute',
+        top: '50px',
         // opacity: '.02',
         width: '100%',
-        height: '700px',
+        height: '650px',
         // margin: 'auto 0',
         display: 'flex',
         // border: 'solid 1px red'
 
     },
     image: {
-        // border: 'solid 1px gray',
         // height: '400px',
         width: '100%',
         maxWidth: '100%',
+        maxHeight: '300px',
         margin: 'auto 0',
-        border: 'solid 1px red'
+        border: 'solid 1px',
+        borderColor: theme.palette.primary.main
     },
     grid: {
-        maxHeight: '300px',
+        maxHeight: '100%',
+        maxWidth: '100%',
+        margin: 'auto auto',
+        padding: '0 10em',
+        // background: 'purple',
         // border: 'solid 1px red'
     }
 }))
@@ -100,22 +119,23 @@ export default function Treatments() {
                     {/* <img className={classes.image} src='/treatment1.svg' alt="site banner" /> */}
                     <Grid className={classes.grid} container spacing={3}>
                         <Grid className={classes.grid} item md={4}>
-                            <img className={classes.image} src='/test.svg' alt="site banner" />
+                            <img className={classes.image} src='/t1.svg' alt="site banner" />
                         </Grid>
                         <Grid className={classes.grid} item md={4}>
-                            <img className={classes.image} src='/test.svg' alt="site banner" />
+                            <img className={classes.image} src='/t2.svg' alt="site banner" />
                         </Grid>
                         <Grid className={classes.grid} item md={4}>
-                            <img className={classes.image} src='/test.svg' alt="site banner" />
+                            <img className={classes.image} src='/t3.svg' alt="site banner" />
+                        </Grid>
+                        {/* ---------------------------------------------------------------- */}
+                        <Grid className={classes.grid} item md={4}>
+                            <img className={classes.image} src='/t4.svg' alt="site banner" />
                         </Grid>
                         <Grid className={classes.grid} item md={4}>
-                            <img className={classes.image} src='/test.svg' alt="site banner" />
+                            <img className={classes.image} src='/t5.svg' alt="site banner" />
                         </Grid>
                         <Grid className={classes.grid} item md={4}>
-                            <img className={classes.image} src='/test.svg' alt="site banner" />
-                        </Grid>
-                        <Grid className={classes.grid} item md={4}>
-                            <img className={classes.image} src='/test.svg' alt="site banner" />
+                            <img className={classes.image} src='/t6.svg' alt="site banner" />
                         </Grid>
                     </Grid>
                 </div>
